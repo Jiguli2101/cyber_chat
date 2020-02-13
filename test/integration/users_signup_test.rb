e437860362f8efe2be481980bd9462c66d15e024
My name is Alexander <18frsevastopol@gmail.com>
2020-02-13 21:14:20 +0200

    Implement basic login

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -10,6 +10,8 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
                                          password:              "foo",
                                          password_confirmation: "bar" } }
     end
+
     assert_template 'users/new'
+
   end
 end
